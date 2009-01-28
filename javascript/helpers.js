633071ab0a29287ad8696b6e8de83b3747ea9c11
Ryan McGeary <ryanongit@mcgeary.org>
2009-01-28 07:18:45 -0500

    cleanup

--- javascript/helpers.js
+++ javascript/helpers.js
@@ -1,23 +1,20 @@
-(function($) {
-  $.fn.sponsor = function(programFile, callback) {
-    var self = this;
-    $.getJSON(programFile, function(program) {
-      var sponsor = program.slots[rand(program.slots.length)];
-      var id = sponsor.id;
-      var anchor = self.find("a");
-      anchor.attr("href", sponsor.url);
-      anchor.find("img").attr("src", sponsor.image);
-      anchor.find("p").html(sponsor.message);
-      if (pageTracker) {
-        pageTracker._trackPageview("/sponsor/" + id);
-        anchor.click(function() { pageTracker._trackPageview("/outgoing/sponsor/" + id); });
-      }
-      if (callback) callback.call(self);
-    });
-    return self;
-  };
-
-})(jQuery);
+$.fn.sponsor = function(programFile, callback) {
+  var self = this;
+  $.getJSON(programFile, function(program) {
+    var sponsor = program.slots[rand(program.slots.length)];
+    var id = sponsor.id;
+    var anchor = self.find("a");
+    anchor.attr("href", sponsor.url);
+    anchor.find("img").attr("src", sponsor.image);
+    anchor.find("p").html(sponsor.message);
+    if (pageTracker) {
+      pageTracker._trackPageview("/sponsor/" + id);
+      anchor.click(function() { pageTracker._trackPageview("/outgoing/sponsor/" + id); });
+    }
+    if (callback) callback.call(self);
+  });
+  return self;
+};
 
 function rand(max) {
   return Math.floor(Math.random() * max);
