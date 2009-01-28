0e169d06210d45a6d47dd02429f19329c174eda6
Ryan McGeary <ryanongit@mcgeary.org>
2009-01-28 17:28:20 -0500

    Stole all the US currency

--- javascript/application.js
+++ javascript/application.js
@@ -1,7 +1,7 @@
 $(function(){
-  var $streamData  = [];
-  var $streamIndex = getStreamIndex();
-  var $streamNode  = $("#stream");
+  var streamData  = [];
+  var streamIndex = getStreamIndex();
+  var streamNode  = $("#stream");
 
   updateData();
 
@@ -10,46 +10,46 @@ $(function(){
   }
 
   function beginStreamRoll(data) {
-    $streamData = data;
+    streamData = data;
     rollStream();
   }
 
   function rollStream() {
-    if($streamIndex > $streamData.length - 1) {
+    if(streamIndex > streamData.length - 1) {
       setStreamIndex(0);
       updateData();
     }
     else {
-      var nextMessage    = $streamData[$streamIndex];
+      var nextMessage    = streamData[streamIndex];
       var nextMessageUrl = "http://lmgtfy.com/?q=" + gentlyEncode(nextMessage);
       var newNode        = $("<li style=\"display:none\"><a></a></li>");
       newNode.find("a")
         .attr("href", nextMessageUrl)
         .text(nextMessage)
-        .click(function(){
-          var $this = $(this);
-          $this.attr("href", "http://vanillaresults.com/?q=" + gentlyEncode(nextMessage));
-          setTimeout(function(){ $this.attr("href", nextMessageUrl); }, 10);
+        .click(function() {
+          var self = $(this);
+          self.attr("href", "http://vanillaresults.com/?q=" + gentlyEncode(nextMessage));
+          setTimeout(function() { self.attr("href", nextMessageUrl); }, 10);
         });
-      $streamNode.append(newNode);
+      streamNode.append(newNode);
       newNode.slideDown();
-      setStreamIndex($streamIndex + 1);
+      setStreamIndex(streamIndex + 1);
       popOffTheTop();
-      setTimeout(function(){ rollStream(); }, rand(3000));
+      setTimeout(rollStream, rand(3000));
     }
   }
 
   function popOffTheTop() {
-    if($streamNode.children().length > 100)
-      popOffTheTop = function() { $streamNode.find("li:first").remove(); };
+    if(streamNode.children().length > 100)
+      popOffTheTop = function() { streamNode.find("li:first").remove(); };
   }
 
   function setStreamIndex(x) {
     $.cookie("streamIndex", x);
-    $streamIndex = x;
+    streamIndex = x;
   }
 
   function getStreamIndex() {
-    return $streamIndex || parseInt($.cookie("streamIndex")) || 0;
+    return streamIndex || parseInt($.cookie("streamIndex")) || 0;
   }
 });
